8f6269e30eb2303a3fe46ba557e6b090052e5436
shriyansh <ishreeyansh@gmail.com>
2020-11-17 12:28:21 +0530

    ok

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ chrome_options.add_argument("--no--sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options)
 
 driver.get("https://www.google.com")
-print(driver.page_source)
+print(driver.page_source) 
